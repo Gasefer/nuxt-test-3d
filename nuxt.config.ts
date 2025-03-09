277adfad2c1067045b8727b9fc06b8be3ac996bd
Oleh <108194510+Gasefer@users.noreply.github.com>
2025-03-09 13:46:09 +0200

    Deploy to GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/image", "@nuxtjs/tailwindcss"],
+  router: {
+    base: "/nuxt-test-3d/",
+  },
 });
