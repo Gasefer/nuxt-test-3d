0d70ce6fc89f67f325825efd031f00ef26b1a777
Oleh <108194510+Gasefer@users.noreply.github.com>
2025-03-09 13:55:27 +0200

    Updates

--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/image", "@nuxtjs/tailwindcss"],
-  router: {
-    base: "/nuxt-test-3d/",
-  },
-});
